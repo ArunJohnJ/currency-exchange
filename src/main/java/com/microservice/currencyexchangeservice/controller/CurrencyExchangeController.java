980d920ab5a927d51c5170ac82a1071781a7fd26
ArunJohnJ <jjohnson.arun@gmail.com>
2022-02-19 16:54:53 +0530

    updating with docker dependencies

--- src/main/java/com/microservice/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/microservice/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,7 +1,7 @@
 package com.microservice.currencyexchangeservice.controller;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +14,8 @@ import com.microservice.currencyexchangeservice.repository.CurrencyExchangeRepos
 @RestController
 public class CurrencyExchangeController {
 	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private Environment environment;
 	
@@ -22,6 +24,7 @@ public class CurrencyExchangeController {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to) {
+		logger.info("getExchangeValue called from from {} to {}",from ,to);
 //		CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(100));
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		if(currencyExchange == null)
